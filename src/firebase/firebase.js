88983a38d731e8f3920819392442aeed0632cbb7
papelnaranja <vanefrenkel@gmail.com>
2021-08-03 09:05:21 -0400

    Separado config de firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,16 +1,6 @@
 import firebase from "firebase/app";
 import 'firebase/firestore';
-
-//firebase config
-var firebaseConfig = {
-    apiKey: "AIzaSyD6h-LtL5AyEs9lKhFmq-Y60SGbfMG2Qsc",
-    authDomain: "orangepaper-coderhouse.firebaseapp.com",
-    projectId: "orangepaper-coderhouse",
-    storageBucket: "orangepaper-coderhouse.appspot.com",
-    messagingSenderId: "1042362089634",
-    appId: "1:1042362089634:web:d0d9aa82cc1aaf6a50d7fb",
-    measurementId: "G-2E5K260G18"
-  };
+import {firebaseConfig} from './config'
 
 const app = firebase.initializeApp(firebaseConfig)
 // inicialize firebase
